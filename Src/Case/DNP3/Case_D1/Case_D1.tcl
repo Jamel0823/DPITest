14376630930639897b3dfdfcb8e9eec7ccde83bc
Cchaha-8213 <kungkung8213@gmail.com>
2020-07-27 15:16:01 +0800

    Modify ouput format

--- Src/Case/DNP3/Case_D1/Case_D1.tcl
+++ Src/Case/DNP3/Case_D1/Case_D1.tcl
@@ -1,13 +1,17 @@
 proc Run_Test_Case_D1 {{Mip} {Sip} {Channel}} {
-  puts "Run_Test_Case_D1"
-
-
+  set testId "Run_Test_Case_D1"
+  MX_TMW_LOG "|---------------------------------------------------------------------|" $testId
+	MX_TMW_LOG "|ID: Run_Test_Case_D1                                                 |" $testId
+	MX_TMW_LOG "|Title: Check DNP3 function case 1: Read Binary Input                 |" $testId
+	MX_TMW_LOG "|Purpose: Test DNP3 function case 1 to be filtered by DNP3 FW         |" $testId
+	MX_TMW_LOG "|DNP3 Master should recevied correct response.                        |" $testId
+	MX_TMW_LOG "|Description:                                                         |" $testId
+	MX_TMW_LOG "|   N/A                                                               |" $testId
+	MX_TMW_LOG "|---------------------------------------------------------------------|" $testId
 
   opendnpmaster "192.168.127.49"
   opendnpslave
-
-  set result "openmbmaster, openmbslave"
-  MX_TMW_LOG $result "Run_Test_Case_D1"
+  MX_TMW_LOG "Step1 openmbmaster 192.168.127.49, openmbslave" $testId
 
   setInterfaceLan
 }
--- Src/Case/Modbus/Case_M1/Case_M1.tcl
+++ Src/Case/Modbus/Case_M1/Case_M1.tcl
@@ -1,7 +1,15 @@
 proc Run_Test_Case_M1 {{Mip} {Sip} {Channel}} {
-  puts "Run_Test_Case_M1"
+  set testId "Run_Test_Case_M1"
+  MX_TMW_LOG "|---------------------------------------------------------------------|" $testId
+  MX_TMW_LOG "|ID: Run_Test_Case_M1                                                 |" $testId
+  MX_TMW_LOG "|Title: Check Modbus function code 0x1: Read Coils Accept             |" $testId
+  MX_TMW_LOG "|Purpose: Test Modbus function code 0x1 to be filtered by modbus FW   |" $testId
+  MX_TMW_LOG "|Modbus Master should recevied correct response.                      |" $testId
+  MX_TMW_LOG "|Description:                                                         |" $testId
+  MX_TMW_LOG "|   N/A                                                               |" $testId
+  MX_TMW_LOG "|---------------------------------------------------------------------|" $testId
 
-  set result "openmbmaster, openmbslave"
-
-  MX_TMW_LOG $result "Run_Test_Case_M1"
+  openmbmaster "192.168.127.49"
+  openmbslave
+  MX_TMW_LOG "openmbmaster, openmbslave" $testId
 }
