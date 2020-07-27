225e4e933a6c1e3644e9a80aab834f3a9c1556ad
Cchaha-8213 <kungkung8213@gmail.com>
2020-07-27 14:30:53 +0800

    modify proc of appendLogInfo

--- Src/Lib/Public/Log/Log.tcl
+++ Src/Lib/Public/Log/Log.tcl
@@ -3,7 +3,7 @@ proc createLogFile {{path}} {
   close $fileID
 }
 
-proc appendLogInfo{{path} {result}} {
+proc appendLogInfo {{path} {result}} {
   set fileID [open $path "a"]
   puts -nonewline $fileID $result
   close $fileID
